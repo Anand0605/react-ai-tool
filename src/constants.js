f575c0967c604341130ac1c87c66dc5769baeb1b
Anand Kumar Gautam <anandvns0510@gmail.com>
2025-06-08 13:54:00 +0530

    update

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,9 @@
-export const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDpmSKlBZEK27mJoh6ss4HBuQcY31ct6ak"
+// export const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDpmSKlBZEK27mJoh6ss4HBuQcY31ct6ak"
+
+export const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${import.meta.env.VITE_API_KEY}`;
+
+console.log('API Key:', import.meta.env.VITE_API_KEY);
+
+
+
+
